fd6a49f533567b410226267c21a738e7bd88a4f8
Ellin Tj <tjellin@gmail.com>
2020-01-30 22:32:35 -0800

    The workday scheduler rows has been successfully generated.

--- script.js
+++ script.js
@@ -7,20 +7,47 @@
     // </div>
 
 // Gathered from style.css, and will be leveraged here.
-    // textarea
-    // .description
-    // .time-block
-    // .row
-    // .hour
-    // .past
-    // .present
-    // .future
-    // .saveBtn
-    // .saveBtn i:hover
+    // .container has multiple .row
+    // .row has .timeBlock, .description and .saveBtn (and .saveBtn i:hover)
+    // .timeBlock has .hour
+    // textarea has .description
+    // .description has .past, .present, and .future
+//----------------------------------------------------
+
+// initialization
+let workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+let $container = $(".container");
+
+// displaying today's Day, Month Date, Year in the header using Moment.js
+$("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'));
+
+$(document).ready(function () {
+    // contents inside the <div class="container">
+    for (let i = 0; i < workHours.length; i++) {
+        // create and append a time-block, description and saveBtn in a textarea row along with their classes
+        let $newRow = $("<div>").attr("class", "row")
+        $container.append($newRow);
+
+        let $timeBlock = $("<div>").attr("class", "timeBlock").text(workHours[i]);
+        $newRow.append($timeBlock);
+
+        let $description = $("<textarea>").attr("class", "description");
+        $newRow.append($description);
+
+        $newRow.append($("<button>").attr("class", "saveBtn"));
 
 
-// displaying today's Day, Month Date, Year in the header
-let currentDate = new Date().toDateString();
-$("#currentDay").text(currentDate);
 
-// contents inside the <div class="container">
+        if (moment()) {
+            $description.attr("class", "past");
+        } else if (moment()) {
+            $description.attr("class", "present");
+        } else {
+            $description.attr("class", "future");
+        }
+
+        console.log("workhours[" + i + "] is " + workHours[i]);
+        // create and append a textarea row into a container
+        $container.append($newRow);
+    }
+});

7b3ae5488a13bacefc1051a8b5c5caff17f0b97f
Ellin Tj <tjellin@gmail.com>
2020-01-30 18:58:45 -0800

    adding comments so I know which elements, classes or ids are available to use or have been used

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Gathered from index.html as reference
     // <header>
     // <p id="currentDay" class="lead"></p>
     // </header>
@@ -5,10 +6,21 @@
     //   <!-- Timeblocks go here -->
     // </div>
 
+// Gathered from style.css, and will be leveraged here.
+    // textarea
+    // .description
+    // .time-block
+    // .row
+    // .hour
+    // .past
+    // .present
+    // .future
+    // .saveBtn
+    // .saveBtn i:hover
 
-    // displaying today's Day, Month Date, Year in the header
-    let currentDate = new Date().toDateString();
-    $("#currentDay").text(currentDate);
 
-    // contents inside the <div class="container">
-    
+// displaying today's Day, Month Date, Year in the header
+let currentDate = new Date().toDateString();
+$("#currentDay").text(currentDate);
+
+// contents inside the <div class="container">

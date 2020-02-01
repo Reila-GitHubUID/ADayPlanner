2d667fb90261eb90d1c88e2d77cc20e1a3530aa1
Ellin Tj <tjellin@gmail.com>
2020-01-31 17:30:33 -0800

    done with making sure localStorage is working properly when users click refresh page

--- script.js
+++ script.js
@@ -24,13 +24,22 @@ $(document).ready(function () {
         $newRow.append($timeBlock);
 
         let $description = $("<textarea>").addClass("description");
-        $description.addClass("present");
-        $description.attr("id", "textarea"+i);
+        $description.addClass("present");  // Note to EL: this is only temporary
+        if (localStorage.getItem("textarea"+i) != null) {
+            var temp = localStorage.getItem("textarea"+i);
+            $description.attr("id", "textarea"+i).text(temp);
+        }
+        else {
+            $description.attr("id", "textarea"+i);
+        }
         $newRow.append($description);
 
         $newRow.append($("<button>").addClass("saveBtn").text("💾").attr("id", "button"+i));
 
+        // create and append a textarea row into a container
+        $container.append($newRow);
 
+        // Adding color coded timeblock to indicate the past, present, and future coloring
         // if (moment()) {
         //     $description.addClass("past");
         // } else if (moment().toDate()) {
@@ -39,64 +48,55 @@ $(document).ready(function () {
         //     $description.addClass("future");
         // }
 
-        // create and append a textarea row into a container
-        $container.append($newRow);
+
     }
 
+    // The button event listeners, and localStorage texts
     $('#button1, #button2, #button3, #button4, #button5, #button6, #button7, #button8, #button9').click(function(event){ 
         if($(event.target).attr('id') === 'button1'){
             let userInput = $.trim($("#textarea1").val());
             $("#textarea1").text(userInput);
-            localStorage.setItem("button1", userInput);
-            console.log("#textarea1 = " + userInput);
+            localStorage.setItem("textarea1", userInput);
         } 
         else if($(event.target).attr('id') ==='button2'){
             let userInput = $.trim($("#textarea2").val());
             $("#textarea2").text(userInput);
-            localStorage.setItem("button2", userInput);
-            console.log("#textarea2 = " + userInput);
+            localStorage.setItem("textarea2", userInput);
         } 
         else if($(event.target).attr('id') ==='button3'){
             let userInput = $.trim($("#textarea3").val());
             $("#textarea3").text(userInput);
-            localStorage.setItem("button3", userInput);
-            console.log("#textarea3 = " + userInput);
+            localStorage.setItem("textarea3", userInput);
         } 
         else if($(event.target).attr('id') ==='button4'){
             let userInput = $.trim($("#textarea4").val());
             $("#textarea4").text(userInput);
-            localStorage.setItem("button4", userInput);
-            console.log("#textarea4 = " + userInput);
+            localStorage.setItem("textarea4", userInput);
         } 
         else if($(event.target).attr('id') ==='button5'){
             let userInput = $.trim($("#textarea5").val());
             $("#textarea5").text(userInput);
-            localStorage.setItem("button5", userInput);
-            console.log("#textarea5 = " + userInput);
+            localStorage.setItem("textarea5", userInput);
         } 
         else if($(event.target).attr('id') ==='button6'){
             let userInput = $.trim($("#textarea6").val());
             $("#textarea6").text(userInput);
-            localStorage.setItem("button6", userInput);
-            console.log("#textarea6 = " + userInput);
+            localStorage.setItem("textarea6", userInput);
         } 
         else if($(event.target).attr('id') ==='button7'){
             let userInput = $.trim($("#textarea7").val());
             $("#textarea7").text(userInput);
-            localStorage.setItem("button7", userInput);
-            console.log("#textarea7 = " + userInput);
+            localStorage.setItem("textarea7", userInput);
         } 
         else if($(event.target).attr('id') ==='button8'){
             let userInput = $.trim($("#textarea8").val());
             $("#textarea8").text(userInput);
-            localStorage.setItem("button8", userInput);
-            console.log("#textarea8 = " + userInput);
+            localStorage.setItem("textarea8", userInput);
         } 
         else if($(event.target).attr('id') ==='button9'){
             let userInput = $.trim($("#textarea9").val());
             $("#textarea9").text(userInput);
-            localStorage.setItem("button9", userInput);
-            console.log("#textarea9 = " + userInput);
+            localStorage.setItem("textarea9", userInput);
         } 
     });
 });
